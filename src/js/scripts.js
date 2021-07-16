c462f2ba20b8a3351cc320534f6873c77a55d9f3
diego <diegop.pin@gmail.com>
2021-07-16 14:01:17 +0100

    traduzo idiomas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,3 +3,42 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+i18next.init({
+    lng: 'en', // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
+    resources: { // evtl. load via xhr https://github.com/i18next/i18next-xhr-backend
+        en: {
+            translation: {
+                nav: {
+                    home: 'homee',
+                    about: 'aboutt',
+                    products: 'products',
+                    store: 'store',
+                },
+            }
+        },
+        es: {
+            translation: {
+                nav: {
+                    home: 'inicio',
+                    about: 'acerca',
+                    products: 'productos',
+                    store: 'tienda',
+                },
+            }
+        },
+    }
+}, function(err, t) {
+    // for options see https://github.com/i18next/jquery-i18next#initialize-the-plugin
+    updateContent();
+    $('.lang-select').on('click', 'a', function(e) {
+        i18next.changeLanguage($(this).data('locale'));
+        updateContent();
+        // $.i18n().locale = $(this).data('locale');
+      });
+});
+
+function updateContent() {
+    jqueryI18next.init(i18next, $); 
+    $('.navbar-nav').localize();
+}
